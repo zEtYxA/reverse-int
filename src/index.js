733227d63eecb49025a1b39f43fa2b84d955e688
zEtYxA <zmaxet@mail.ru>
2022-08-04 01:37:00 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let s = '';
+   if (n < 0) { n = n * (-1) }
+   s = String(n).split("").reverse().join("")
+
+   return s
 }
+
